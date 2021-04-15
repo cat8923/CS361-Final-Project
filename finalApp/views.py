d9652bec40be6a93a0056683ed26e5496403a896
jacob <jdobratz@uwm.edu>
2021-04-15 15:21:57 -0500

    Initial attempt at HTML and some of views

--- finalApp/views.py
+++ finalApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+
+class Login(View):
+    def get(self, request):
+        return render(request, "Login.html")
+
+    def post(self, request):
+        NoSuchUser = False
+        badPassword = False
+        try:
+            # check to see if there is a user with the chosen name
+
+        except:
+            noSuchUser=True
+
+        if noSuchUser:
+            return render(request, "Login.html", {"message":"Username not recognized."})
+        elif badPassword:
+            return render(request, "Login.html", {"message":"Password incorrect"})
+        else:
+            request.session["username"] = user.username
+            return redirect("/Homepage/")
+
+
+
+
+class Homepage(View):
+    def get(self, request):
+        return render(request, "Homepage.html")
+
+    def post(self, request):
+        click = request.POST['onclick']
+        if click == 'accounts':
+            return redirect("/accounts/")
+        elif click == 'courses':
+            return redirect("/courses/")
+        elif click == 'logout':
+            return render(request, "Login.html")
