8968d34ee8db65378f2dfe9a49257ba5867fdbf9
Spencer Ladd <srladd@uwm.edu>
2021-04-15 15:43:58 -0500

    Setting up files for tests, experimenting with django User.

--- finalApp/models.py
+++ finalApp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
-
+from django.contrib.auth import models
 # Create your models here.
 
+
+class MyUser(models.User):
+    pass
--- finalApp/tests.py
+++ finalApp/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import MyUser
 # Create your tests here.
+
+
+class DbCreateTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_createUser(self):
+        temp = MyUser(username="user1", first_name="john", last_name="doe")
+        temp.set_password(raw_password="pass1")
+        temp.save()
+        print()
