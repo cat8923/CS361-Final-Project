dd08927bcb19a03afea36a3fd3d49a37aeec426b
Sai Indukuri <60232892+indukuri04@users.noreply.github.com>
2021-04-24 16:00:38 -0500

    Update test_createaccount.py

--- finalApp/test_createaccount.py
+++ finalApp/test_createaccount.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.test import Client
-from .models import MyUser, UserType
+from Final_Project.models import MyUser, UserType,CourseData, CourseSections
 
 
 class createAccount(TestCase):
@@ -10,26 +10,40 @@ class createAccount(TestCase):
         self.Client.session.save()
 
         # creates 3 users (user0, user1, user2)
-        for i in range(3):
-            temp = MyUser(username="user" + str(i), first_name="john" + str(i), last_name="deer" + str(i))
-            temp.set_password(raw_password="pass" + str(i))
-            temp.save()
+        # for i in range(3):
+        self.temp = MyUser(username="user", first_name="john", last_name="deer", position="S")
+        self.temp.set_password(raw_password="pass")
+        self.temp.save()
 
+        self.data = CourseData(title="Cs")
+
+        self.course = CourseSections(course=self.data, section=901)
 
     def test_noAccount(self):
-        response = self.Client.post("/create_account/", {"username": "bic21", "password": "hello", "first_name": "brett",
-                                                         "last_name": "frank", "address": "3423 N Maryland",
-                                                         "title": UserType.SUPERVISOR, "email": "test@test.com",
-                                                         "number": "123456789"})
-        self.assertEqual(response.context["message"], "successfully created account", msg="confirmed account creation")
-        self.assertEqual(response.url, "/edit_account/")
+        response1 = self.client.post("/Login/", {'name': 'user', 'password': 'pass'})
+        self.assertEqual("/HomePage/", response1.url, "Valid Information will take to the homepage page")
 
+        response = self.Client.post("/create_account/",
+                                    {"username": "bic21", "password": "hello", "first_name": "brett",
+                                     "last_name": "frank", "address": "3423 N Maryland",
+                                     "title": "I", "email": "test@test.com",
+                                     "number": "123456789"})
+
+        self.assertEqual(response.context["message"], "successfully created account", msg="confirmed account creation")
+        self.assertEqual(response.url, "/HomePage/")
+
+        # assign the user created to the course declared in setUp
+        response2 = self.Client.post("/account/", {"users": 1})
+        self.assertEqual(response2.url, "/Assign_Course/")
+
+        response3 = self.Client.post("/Assign_Course/", {"courses": 1})
+        self.assertEqual(response3.url, "/HomePage/")
 
     def test_accountExists(self):
-        response = self.Client.post("/create_account/", {"username": "user1", "password": "pass1", "first_name": "brett",
-                                                         "last_name": "frank", "address": "3423 N Maryland",
-                                                         "title": UserType.SUPERVISOR, "email": "test@test.com",
-                                                         "number": "123456789"})
+        response = self.Client.post("/create_account/",
+                                    {"username": "user1", "password": "pass1", "first_name": "brett",
+                                     "last_name": "frank", "address": "3423 N Maryland",
+                                     "title": UserType.SUPERVISOR, "email": "test@test.com",
+                                     "number": "123456789"})
         self.assertEqual(response.context["message"], "account already exists", msg="account was not created")
         self.assertEqual(response.url, "/create_account/")
-        
