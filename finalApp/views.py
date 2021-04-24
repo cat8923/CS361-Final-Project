4618a6cb195a26365e4899c8329f20285abeee21
Sai Indukuri <60232892+indukuri04@users.noreply.github.com>
2021-04-24 16:36:17 -0500

    Update views.py

--- finalApp/views.py
+++ finalApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, user
+from django.views import View
 
 # Create your views here.
+
+class LabScreen(View):
+    def get(self, request):
+        return render(request, "create_Lab.html")
+
+    def post(self, request):
+        NoSuchUser = False
+        badPassword = False
+        try:
+            # check to see if there is a user with the chosen name
+
+        except:
+            noSuchUser=True
+
+        if noSuchUser:
+            return render(request, "create_Lab.html", {"message":"Username not recognized."})
+        elif badPassword:
+            return render(request, "create_Lab.html", {"message":"Password incorrect"})
+        else:
+            request.session["username"] = user.username
+            return redirect("/create_Lab/")
+
+
+
+
+class Homepage(View):
+    def get(self, request):
+        return render(request, "create_Lab.html")
+
+    def post(self, request):
+        click = request.POST['onclick']
+        if click == 'edit':
+            return redirect("/editLab/")
+        elif click == 'Logout':
+            return redirect("/Login/")
+        elif click == 'logout':
+
