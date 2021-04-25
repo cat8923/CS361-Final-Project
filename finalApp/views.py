4484911ed72009874fe63329a249d1eaaec19e87
Sai Indukuri <60232892+indukuri04@users.noreply.github.com>
2021-04-25 14:47:59 -0500

    Update views.py

--- finalApp/views.py
+++ finalApp/views.py
@@ -1,41 +1,43 @@
-from django.shortcuts import render, redirect, user
+from django.shortcuts import render, redirect
 from django.views import View
+from Final_Project.models import MyUser
 
 # Create your views here.
 
-class LabScreen(View):
+class AddLab(View):
     def get(self, request):
-        return render(request, "create_Lab.html")
+        return render(request, "createLab")
+
+    def post(self, request):
+        noSuchUser = False
+        badPassword = False
+
+        if noSuchUser:
+            return render(request, "createLab", {"message":"Username not recognized."})
+        elif badPassword:
+            return render(request, "createLab", {"message":"Password incorrect"})
+        else:
+            request.session["username"] = MyUser.username
+            return redirect("createLab")
+
+class EditLab(View):
+    def get(self, request):
+        return render(request, "createLab")
 
     def post(self, request):
         NoSuchUser = False
         badPassword = False
         try:
             # check to see if there is a user with the chosen name
-
+            # create a variable with that person's details
+            # redirect to create lab and save all information
         except:
             noSuchUser=True
 
         if noSuchUser:
-            return render(request, "create_Lab.html", {"message":"Username not recognized."})
+            return render(request, "createLab", {"message":"Username not recognized."})
         elif badPassword:
-            return render(request, "create_Lab.html", {"message":"Password incorrect"})
+            return render(request, "createLab", {"message":"Password incorrect"})
         else:
-            request.session["username"] = user.username
-            return redirect("/create_Lab/")
-
-
-
-
-class Homepage(View):
-    def get(self, request):
-        return render(request, "create_Lab.html")
-
-    def post(self, request):
-        click = request.POST['onclick']
-        if click == 'edit':
-            return redirect("/editLab/")
-        elif click == 'Logout':
-            return redirect("/Login/")
-        elif click == 'logout':
-
+            request.session["username"] = MyUser.username
+            return redirect("createLab")
