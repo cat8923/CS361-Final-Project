61aa8e6c32a486f6e14928156daafbb65e8efc07
kjmaaske <kjmaaske@uwm.edu>
2021-04-26 22:44:49 -0500

    Course List Views

--- finalApp/views.py
+++ finalApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.views import View
 from finalApp import database_access
 
@@ -7,7 +7,7 @@ class CreateCourse(View):
     def get(self,request):
         if len(request.GET) == 0:
             TA = list(filter(lambda x: x[1] == 'T', database_access.list_users()))
-            return render(request, "create_course.html", {"TA":TA})
+            return render(request, "create_course.html", {"TA": TA})
 
     def post(self,request):
         courseDict = {
@@ -18,10 +18,10 @@ class CreateCourse(View):
 
 
 class CourseList(View):
-    def get(self,request):
+    def get(self, request):
         if len(request.GET) == 0:
             courses = list(database_access.list_courses())
-            return render(request, "course_list.html", {"courses":courses})
+            return render(request, "course_list.html", {"courses": courses})
 
 
 
