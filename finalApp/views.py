c8b4f008c99eefc51f56592168d69bcdb4da6049
Sai Indukuri <60232892+indukuri04@users.noreply.github.com>
2021-04-26 23:31:04 -0500

    Sai (#7)
    * Update test_createaccount.py
    
    * Create test_createCourses
    
    * Create test_login
    
    * Create test_createCourses
    
    * Create test_createLab
    
    * Create test_editLab
    
    * Create test_editAccount
    
    * Create LabScreen.html
    
    * Delete test_createCourses
    
    * Create test_AssignTA
    
    * Update views.py
    
    * Update test_AssignTA
    
    * Update test_login
    
    * Update test_AssignTA
    
    * Update test_createLab
    
    * Update test_editLab
    
    * Create test_editCourse
    
    * Update test_AssignTA
    
    * Update views.py
    
    * Update LabScreen.html
    
    * Update urls.py
    
    * Update views.py
    
    * Update and rename test_createaccount.py to test_createAccount.py
    
    * Update test_login
    
    * Update test_createLab
    
    * Update test_editCourse
    
    * Update test_createCourses
    
    * Update test_AssignTA
    
    * Update LabScreen.html
    
    * Update views.py
    
    * Delete test_editLab
    
    * Rename test_AssignTA to test_AssignTA.py
    
    * Rename test_createCourses to test_createCourses.py
    
    * Rename test_createLab to test_createLab.py
    
    * Rename test_editAccount to test_editAccount.py
    
    * Rename test_editCourse to test_editCourse.py
    
    * Rename test_login to test_login.py
    
    * Update test_AssignTA.py
    
    * Update test_createAccount.py
    
    * Update test_createCourses.py
    
    * Update test_createLab.py
    
    * Update test_editAccount.py
    
    * Update test_login.py
    
    Co-authored-by: Spencer Ladd <55863911+cat8923@users.noreply.github.com>

--- finalApp/views.py
+++ finalApp/views.py
@@ -48,6 +48,20 @@ class CreateCourse(View):
         }
         database_access.make_course(courseDict)
 
+        
+class AddLab(View):
+        def get(self, request):
+            if len(request.GET) == 0:
+                TA = list(filter(lambda x: x[1] == 'T', database_access.list_users()))
+                return render(request, "/Create_Lab/", {"TA": TA})
+
+        def post(self, request):
+            labDict = {
+                "courseID": request.GET["description"],
+                "section": request.GET["designation"],
+            }
+            database_access.make_lab(labDict)
+
 
 class CourseList(View):
     def get(self, request):
