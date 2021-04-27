7419afd131f2f11839dc2ce96add160b80e5ad99
Sai Indukuri <60232892+indukuri04@users.noreply.github.com>
2021-04-26 23:02:10 -0500

    Update test_login.py

--- finalApp/test_login.py
+++ finalApp/test_login.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test import Client
 from Final_Project.models import MyUser
 
-class login(TestCase):
+class testLogin(TestCase):
     def setUp(self):
             self.Client = Client()
             self.Client.session["position"] = "S"
