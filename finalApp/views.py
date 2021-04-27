4538b718df22e5d0895ca178edde334db6c25eb7
Sai Indukuri <60232892+indukuri04@users.noreply.github.com>
2021-04-26 20:34:45 -0500

    Update views.py

--- finalApp/views.py
+++ finalApp/views.py
@@ -20,24 +20,4 @@ class AddLab(View):
             request.session["username"] = MyUser.username
             return redirect("createLab")
 
-class EditLab(View):
-    def get(self, request):
-        return render(request, "createLab")
 
-    def post(self, request):
-        NoSuchUser = False
-        badPassword = False
-        try:
-            # check to see if there is a user with the chosen name
-            # create a variable with that person's details
-            # redirect to create lab and save all information
-        except:
-            noSuchUser=True
-
-        if noSuchUser:
-            return render(request, "createLab", {"message":"Username not recognized."})
-        elif badPassword:
-            return render(request, "createLab", {"message":"Password incorrect"})
-        else:
-            request.session["username"] = MyUser.username
-            return redirect("createLab")
