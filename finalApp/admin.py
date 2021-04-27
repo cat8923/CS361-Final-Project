53da8d355c14959ecf3c1877ef7570308bf50e6f
jacob <jdobratz@uwm.edu>
2021-04-26 21:33:43 -0500

    Added admin and updated urls and settings.py

--- finalApp/admin.py
+++ finalApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import MyUser, UserType, CourseData, CourseSections, LabData, TAsToCourses
+admin.site.register(MyUser)
+admin.site.register(CourseData)
+admin.site.register(CourseSections)
+admin.site.register(LabData)
+admin.site.register(TAsToCourses)
