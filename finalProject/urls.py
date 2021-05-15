bdb439f75f4adc9b570d4d6ae123910a7516ae77
jdobratz <70720914+jdobratz@users.noreply.github.com>
2021-05-15 16:36:16 -0500

    Jake new branch (#20)
    * Changed colors on base template and added account list to base.
    
    * Applied template to the account html pages
    
    * Deleted comments
    
    Co-authored-by: Spencer Ladd <55863911+cat8923@users.noreply.github.com>

--- finalProject/urls.py
+++ finalProject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from finalApp.views import Login, Homepage, CreateCourse, CourseList, CreateAccount, Blank, Logout, TestCreate, EditAccount, AccountList, EditCourse
+from finalApp.views import Login, Homepage, CreateCourse, CourseList, CreateAccount, Blank, Logout, TestCreate, EditAccount, AccountList, EditCourse, AccountView
 import finalApp.views as fav
 
 urlpatterns = [
